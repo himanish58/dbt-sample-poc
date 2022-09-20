fc2fa169127429c2d258f981cd12dc61ab7b5260
f20160464h <109265809+f20160464h@users.noreply.github.com>
2022-09-20 15:05:19 +0530

    Update get_relevant_details_for_countries.sql

--- demo_dbt_redshift/models/test_models/get_relevant_details_for_countries.sql
+++ demo_dbt_redshift/models/test_models/get_relevant_details_for_countries.sql
@@ -1,4 +1,4 @@
 
-SELECT name, subregion, phone_code, capital, currency, __hevo__ingested_at/1000 as
+SELECT name, region, __hevo__ingested_at/1000 as
 __hevo__ingested_at FROM {{ref("get_countries_by_region")}}
 
